7267ee40bb221784d032f2e48a1a22dd217a70b0
Harish Narayanan <hnarayanan@gmail.com>
2014-11-02 18:20:00 +0000

    Replace $http with $resource

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,9 @@
  * Controller of the cheeperApp
  */
 angular.module('cheeperApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http({method: 'GET', url: 'http://127.0.0.1:8000/cheeps/'}).
-      success(function(data) {
-        $scope.firehose = data;
-      }).
-      error(function() {
-    });
+  .controller('MainCtrl', function ($scope, $resource) {
+      var Cheep = $resource('http://127.0.0.1:8000/cheeps/');
+      var cheeps = Cheep.query(function() {
+	  $scope.firehose = cheeps;
+      });
   });
